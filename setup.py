078cc55e340ce1cf46b75106453236f1043f4a66
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-11-14 15:49:21 +0000

    Add readme to pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pythonlog',
     url='https://github.com/awesomelewis2007/pylog/',
@@ -7,7 +10,9 @@ setup(
     author_email='awesomelewis2007@gmail.com',
     packages=['pylog'],
     install_requires=[''],
-    version='0.1.2',
+    version='0.1.3',
     license='GNU',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description='The new python logger'
 )
